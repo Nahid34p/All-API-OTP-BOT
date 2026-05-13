d787c2ddf9f9a0a6c1bfdf0a58571cef8079587f
Nahid34p <nsnahid34p@gmail.com>
2026-05-13 14:13:45 +0600

    Add configuration settings for Telegram and API

---
+++ config.py
@@ -0,0 +1,10 @@
+# ——————————————— TELEGRAM ———————————————
+BOT_TOKEN = "8791565201:AAHepdm5e8eyjmUpHJJ5OaLe4eHTOsO76nc"
+CHAT_ID   = "-1002745826680"
+# ——————————————— API ———————————————
+API_URL = "http://147.135.212.197/crapi/s1t/viewstats"
+TOKEN   = "RlBYQjRSQmhzUXZZXGx1SWmElHiEU4hehpiDYHuPcHVJa2V6Qk6S"
+# ——————————————— BUTTON URLS ———————————————
+CHANNEL_URL = "https://t.me/nsfbnbo"
+GROUP_URL = "https://t.me/otp_group_ns"
+DEVELOPER_URL = "https://t.me/nahid34p_admin"
